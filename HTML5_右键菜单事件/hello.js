0e4601562ac5ede56321bce2acbd4b531a4fcb7a
Cloudox <cloudox@foxmail.com>
2016-10-19 10:54:28 +0800

    HTML5右键菜单事件

---
+++ HTML5_右键菜单事件/hello.js
@@ -0,0 +1,12 @@
+var div = document.getElementById("myDiv");
+div.addEventListener("contextmenu", function(event) {// 右键显示菜单
+	var menu = document.getElementById("myMenu");
+	menu.style.left = event.clientX + "px";
+	menu.style.top = event.clientY + "px";
+	menu.style.visibility = "visible";
+});
+
+document.addEventListener("click", function(event) {// 点击屏幕隐藏菜单
+	document.getElementById("myMenu").style.visibility = "hidden";
+});
+
